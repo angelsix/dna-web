db0266e8dcad0b9e85dba8f6f0f380ec476486a1
Unknown <contact@angelsix.com>
2017-02-23 18:18:22 +0000

    Variables and profiles

---
+++ Source/Dna.HtmlEngine.Core/Engines/Base/Data/FileProcessingData.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace Dna.HtmlEngine.Core
+{
+    /// <summary>
+    /// Information about a file as it is being processed, for use throughout the stages
+    /// </summary>
+    public class FileProcessingData
+    {
+        /// <summary>
+        /// The full path of the file
+        /// </summary>
+        public string FullPath { get; set; }
+
+        /// <summary>
+        /// Indicates if this file is a partial file
+        /// NOTE: Partial files don't generate output themselves
+        /// </summary>
+        public bool IsPartial { get; set; }
+
+        /// <summary>
+        /// A list of variables for the file
+        /// </summary>
+        public List<EngineVariable> Variables { get; set; } = new List<EngineVariable>();
+
+        /// <summary>
+        /// A list of output file data
+        /// </summary>
+        public List<FileOutputData> OutputPaths { get; set; } = new List<FileOutputData>();
+
+        /// <summary>
+        /// The files full contents, that get's edit during the course of being edit
+        /// </summary>
+        public string FileContents { get; set; }
+
+        /// <summary>
+        /// Information about any error in processing the file
+        /// </summary>
+        public string Error { get; set; }
+
+        /// <summary>
+        /// Indicates if the processing of this file is still ok and has not had an error
+        /// </summary>
+        public bool Successful => string.IsNullOrEmpty(Error);
+    }
+}

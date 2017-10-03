25afe01269092af6864b7e3da5dc590d0a2b5412
Unknown <contact@angelsix.com>
2017-10-03 16:58:38 +0100

    Installer upgrade to self-contained heat installer

--- Source/Dna.Web.CommandLine/dnaweb.cmd
+++
@@ -1,4 +0,0 @@
-@echo off 
-REM Helper script to run DnaWeb from commandline by typing the name of this script without the .cmd extension
-REM Install this into a PATH location to invoke DnaWeb from anywhere
-dotnet "%~dp0Dna.Web.CommandLine.dll
